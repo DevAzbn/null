6d3ebc6b45bba3e04e323d7a49904dd2304e2035
Alexander <alexander.zybin@gmail.com>
2017-01-19 15:16:06 +0300

    201701191516

---
+++ javascript/ddos/index.js
@@ -0,0 +1,38 @@
+/*
+var i = 1;
+
+(function(_){
+	
+	_(_);
+	
+})(function(_){
+	
+	console.log(i);
+	
+	i++;
+	
+	_(_);
+	
+})
+*/
+
+/*
+var i = 1;
+var a;
+
+a = function() {
+	
+	console.log(i);
+	
+	i++;
+	
+	a();
+	
+};
+
+(function(){
+	
+	a();
+	
+})();
+*/
