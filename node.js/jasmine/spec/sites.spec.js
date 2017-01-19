0999fec25004e0622b8736afc10b27a22c121d11
Alexander <alexander.zybin@gmail.com>
2017-01-19 16:52:03 +0300

    201701191652

---
+++ node.js/jasmine/spec/sites.spec.js
@@ -0,0 +1,43 @@
+
+var _
+	, request = require('request')
+	//, jasmine = require('jasmine-node')
+;
+
+describe('Доступность сайтов azbn.ru', function(){
+	
+	var sites = [
+		'https://azbn.ru/',
+		'http://bercan.ru/',
+	];
+	
+	for(var i in sites) {
+		it(sites[i], function(done) {
+			request(sites[i], function(error, response, body){
+				expect(response.statusCode).toBe(200);
+				done();
+			});
+		});
+	}
+	
+});
+
+describe('Доступность сайтов dorohovdesign.ru', function(){
+	
+	var sites = [
+		'http://dorohovdesign.ru/',
+		'http://yarus57.ru/',
+		'http://fe.enerfit.ru/',
+		'http://endurancerobots.com/',
+	];
+	
+	for(var i in sites) {
+		it(sites[i], function(done) {
+			request(sites[i], function(error, response, body){
+				expect(response.statusCode).toBe(200);
+				done();
+			});
+		});
+	}
+	
+});
